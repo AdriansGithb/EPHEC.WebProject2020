06ae4b02ea0785e3a7b1cd55e2fb5065e7e1ad64
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-13 13:21:15 +0200

    IS4 : Quickstart2 : Adding the MVC Client to the IdentityServer Configuration

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -18,21 +19,35 @@ namespace IdentityServer
         public static IEnumerable<Client> Clients =>
             new List<Client>
             {
+                // machine to machine client (from quickstart 1)
                 new Client
                 {
                     ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
 
-                    // no interactive user, use the clientid/secret for authentication
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                    // secret for authentication
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-
                     // scopes that client has access to
                     AllowedScopes = { "MyAPI" }
+                },
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
 
