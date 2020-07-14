b6b88900eb53166110a8cb0cc99c9275817f660a
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-14 11:51:37 +0200

    +MyConstants

---
+++ MyConstants/MyAPIConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConstants
+{
+    public static class MyAPIConstants
+    {
+        public const string MyAPI_url = "";
+    }
+}
---
+++ MyConstants/MyIdentityServerConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConstants
+{
+    public static class MyIdentityServerConstants
+    {
+        public const string IS_url = "https://localhost:44318/";
+
+
+    }
+}
