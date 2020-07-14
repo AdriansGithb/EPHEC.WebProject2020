abc33ecf9960ad482e7fb221d3f0c9e9c5d2369a
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-14 13:02:25 +0200

    IS4-Q2 : Creating an MVC client

---
+++ MyConstants/MyMVCConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConstants
+{
+    public static class MyMVCConstants
+    {
+        public const string MyMVC_Name = "MVCClient";
+
+        public const string MyMVC_Url = "https://localhost:44352/";
+    }
+}
