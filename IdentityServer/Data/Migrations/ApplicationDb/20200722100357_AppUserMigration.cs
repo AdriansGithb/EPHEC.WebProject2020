0afc60b41ecd1181a50245191287c7a70f1aa333
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-22 12:12:34 +0200

    ApplicationUser : properties added + migration done (in IdentityServer) + db updated

---
+++ IdentityServer/Data/Migrations/ApplicationDb/20200722100357_AppUserMigration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Data.Migrations.ApplicationDb
+{
+    public partial class AppUserMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GenderId",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsProfessional",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GenderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsProfessional",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- MyLibrary/Models/ApplicationUser.cs
+++ MyLibrary/Models/ApplicationUser.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace MyLibrary.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int GenderId { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsProfessional { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
