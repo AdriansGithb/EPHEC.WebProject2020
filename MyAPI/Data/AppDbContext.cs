2a9d63b8f205f68f0d57d18768795f47f6f698c7
VANHOEKE Adrian <PSR10172@students.ephec.be>
2020-07-23 08:41:49 +0200

    save before test moving dbcontexts

--- MyAPI/Data/AppDbContext.cs
+++ MyAPI/Data/AppDbContext.cs
@@ -17,30 +17,11 @@ namespace MyAPI.Data
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {            
-            //modelBuilder
-            //    .Entity<ApplicationUser>()
-            //    .Property(e => e.GenderType_Id)
-            //    .HasConversion<int>();
-
-            //modelBuilder
-            //    .Entity<GenderTypesModel>()
-            //    .Property(e => e.Id)
-            //    .HasConversion<int>();
-
-            //modelBuilder
-            //    .Entity<GenderTypesModel>().HasData(
-            //        Enum.GetValues(typeof(GenderTypesId))
-            //            .Cast<GenderTypesId>()
-            //            .Select(e => new GenderTypesModel()
-            //            {
-            //                Id = e,
-            //                Name = e.ToString()
-            //            })
-            //    );
+        { 
+            
         }
-        //private DbSet<ApplicationUser> aspNetUsers { get; set; }
-        //private DbSet<GenderTypesModel> Gender_Types { get; set; }
+
+        public DbSet<ApplicationUser> AspNetUsers { get; set; }
 
     }
 }
---
+++ MyAPI/Data/Migrations/20200722154200_InitialMigrationAppDbCtx.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyAPI.Data.Migrations
+{
+    public partial class InitialMigrationAppDbCtx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
